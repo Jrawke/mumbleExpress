d05cd79f22e1b19e2cc79922b659ed407a037938
Owen Horwitz <ohorwitz@ucla.edu>
2015-10-01 16:33:58 -0700

    channels can now be removed from tree. fixed race condition with multiple calls to digest()

--- public/main.js
+++ public/main.js
@@ -1,8 +1,27 @@
 var app = angular.module('mumbleExpressApp', ['luegg.directives']);
 
-app.factory('socket', function(){
+app.factory('socket', function ($rootScope) {
     var socket = io();
-    return socket;
+    return {
+	on: function (eventName, callback) {
+	    socket.on(eventName, function () {
+		var args = arguments;
+		$rootScope.$apply(function () {
+		    callback.apply(socket, args);
+		});
+	    });
+	},
+	emit: function (eventName, data, callback) {
+	    socket.emit(eventName, data, function () {
+		var args = arguments;
+		$rootScope.$apply(function () {
+		    if (callback) {
+			callback.apply(socket, args);
+		    }
+		});
+	    })
+	}
+    };
 });
 
 function insertChannelIntoTree(node, tree) {
@@ -21,6 +40,21 @@ function insertChannelIntoTree(node, tree) {
     for (child of tree.childChannels) {
 	if(insertChannelIntoTree(node,child))
 	    return true;
+    }
+
+    return false;
+}
+
+function deleteChannelFromTree(channelId, tree) {
+    var i=0;
+    for (childChannel of tree.childChannels) {
+	if(childChannel.channel == channelId) {
+	    tree.childChannels.splice(i,1);
+	    return true;
+	}
+	else if(deleteChannelFromTree(channelId,childChannel))
+	    return true;
+	i++;
     }
 
     return false;
@@ -100,7 +134,6 @@ app.controller('mumbleExpressController', function($scope, socket){
 	}
 	$scope.msgs.push(textMessage);
 	$scope.msg.text = '';
-	$scope.$digest();
     };
 
     socket.on('textMessage',function(textMessage) {
@@ -110,14 +143,10 @@ app.controller('mumbleExpressController', function($scope, socket){
 	textMessage["time"]=''+d.getHours()+':'+d.getMinutes();
 
 	$scope.msgs.push(textMessage);
-	$scope.$digest();
     });
 
     socket.on('userState', function(state) {
-	console.log("userState");
-	console.log(state);
 	if(state.name) { // a new user connected
-	    console.log('adding new user');
 	    if(state.channel_id == null) {
 		//make those in the root channel a child of the
 		//root node for cleaner rendering. Why doesn't
@@ -126,46 +155,38 @@ app.controller('mumbleExpressController', function($scope, socket){
 	    }
 	    insertUserIntoTree(state,$scope.channelTree);
 	}
-	else { //updating something about user info
-	    if(state.channel_id!=null) { //updating user position
-		console.log('moving user position');
-		user = getUserFromTree(state.session,$scope.channelTree);
-		deleteUserFromTree(state.session,$scope.channelTree);
-		user.channel_id=state.channel_id;
-		insertUserIntoTree(user,$scope.channelTree);
-	    }
-	    if(state.self_mute!=null) { //updating user mute/deaf
-		console.log('updating mute/def');
-		user = getUserFromTree(state.session,$scope.channelTree);
-		deleteUserFromTree(state.session,$scope.channelTree);
-		user.self_mute=state.self_mute;
-		user.self_deaf=state.self_deaf;
-		insertUserIntoTree(user,$scope.channelTree);
-	    }
-	}
-	$scope.$digest();
-	console.log($scope.channelTree);
+	
+	//update user info
+	user = getUserFromTree(state.session,$scope.channelTree);
+	if(state.channel_id!=null) //updating user position
+	    user.channel_id=state.channel_id;
+
+	if(state.self_deaf==true) //user deafened, must be mute also
+	    user.self_deaf = user.self_mute = true;
+
+	if(state.self_deaf==false) //user deafened
+	    user.self_deaf = false;
+
+	if(state.self_mute!=null) //updating user mute
+	    user.self_mute=state.self_mute;
+
+	//replace user with updated version in tree
+	deleteUserFromTree(state.session,$scope.channelTree);
+	insertUserIntoTree(user,$scope.channelTree);
     });
 
     socket.on('channelState', function(state) {
-	console.log("channelState");
-	console.log(state);
 	insertChannelIntoTree(state,$scope.channelTree);
-	$scope.$digest();
-	console.log($scope.channelTree);
     });
 
     socket.on('channelRemove', function(state) {
 	console.log("channelRemove");
 	console.log(state);
+	deleteChannelFromTree(state.channel_id,$scope.channelTree);
     });
 
     socket.on('userRemove', function(state) {
-	console.log("userRemove");
-	console.log(state);
 	deleteUserFromTree(state.session,$scope.channelTree);
-	$scope.$digest();
-	console.log($scope.channelTree);
     });
 
 });
