5bd9eaa85a990ea2bd125bbd5ad31c47d8ff801b
Owen Horwitz <ohorwitz@ucla.edu>
2015-12-17 21:54:41 -0800

    basic build automation set up

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,31 +1,41 @@
 var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     concat = require('gulp-concat'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    compass = require('gulp-compass');
 
-// Browserify task
 gulp.task('browserify', function() {
-    // Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
     gulp.src(['client/scripts/main.js'])
 	.pipe(browserify({
 	    insertGlobals: true,
 	    debug: true
 	}))
-    // Bundle to a single file
 	.pipe(concat('bundle.js'))
-    // Output it to our dist folder
 	.pipe(gulp.dest('dist/js'));
 });
 
-// Views task
 gulp.task('views', function() {
-    // Get our index.html
     gulp.src('client/index.html')
-    // And put it in the dist folder
 	.pipe(gulp.dest('dist/'));
 
-    // Any other view files from app/views
     gulp.src('./client/views/**/*')
-    // Will be put in the dist/views folder
 	.pipe(gulp.dest('dist/views/'));
 });
+
+gulp.task('compass', function() {
+    gulp.src('./client/styles/sass/*')
+        .pipe(compass({
+	    config_file: './client/styles/config.rb',
+	    css: './dist/',
+	    sass: './client/styles/sass/'
+	}))
+        .pipe(gulp.dest('dist/assets/'));
+
+    gulp.src('./client/styles/icons/*')
+	.pipe(gulp.dest('dist/icons'));
+});
+
+gulp.task('default', function() {
+    gulp.run('browserify', 'views', 'compass');
+});
+	  
--- client/styles/config.rb
+++ client/styles/config.rb
@@ -3,11 +3,11 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "/"
-sass_dir = "scss"
-images_dir = "icons"
-javascripts_dir = "js"
+http_path = "./"
+css_dir = "./"
+sass_dir = "./sass"
+images_dir = "./imgs"
+javascripts_dir = "./js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
