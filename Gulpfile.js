65c66abe7f5f43326e796b7ad3ef86dca2b9eaf5
Owen Horwitz <ohorwitz@ucla.edu>
2015-12-18 14:13:14 -0800

    added gulp clean and angular-ui-tree css

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,7 +5,14 @@ var gulp = require('gulp'),
     compass = require('gulp-compass'),
     bower = require('gulp-bower');
 
-gulp.task('browserify', ['bower'], function() {
+//clean dist directory
+gulp.task('clean', function() {
+    return gulp.src('dist', {read: false})
+	.pipe(clean());
+});
+
+//bundle js files for release
+gulp.task('browserify', ['angular-ui-tree'], function() {
     gulp.src(['client/scripts/main.js'])
 	.pipe(browserify({
 	    insertGlobals: true,
@@ -15,6 +22,7 @@ gulp.task('browserify', ['bower'], function() {
 	.pipe(gulp.dest('dist/js'));
 });
 
+//move angular views into release directory
 gulp.task('views', function() {
     gulp.src('client/index.html')
 	.pipe(gulp.dest('dist/'));
@@ -23,23 +31,30 @@ gulp.task('views', function() {
 	.pipe(gulp.dest('dist/views/'));
 });
 
+//preprocess CSS
 gulp.task('compass', function() {
     gulp.src('./client/styles/sass/*.scss')
         .pipe(compass({
 	    config_file: './client/styles/config.rb',
-	    css: './dist/',
+	    css: './dist/assets/',
 	    sass: './client/styles/sass/'
 	}))
         .pipe(gulp.dest('dist/assets/'));
 
     gulp.src('./client/styles/icons/*')
-	.pipe(gulp.dest('dist/icons'));
+	.pipe(gulp.dest('dist/assets/icons'));
+});
+
+
+//install angular-ui-tree
+gulp.task('angular-ui-tree', ['bower'], function () {
+    gulp.src('bower_components/angular-ui-tree/dist/angular-ui-tree.min.css')
+	.pipe(gulp.dest('dist/assets'));
 });
 
 gulp.task('bower', function() {
-    return bower()
+    return bower();
 });
 
-gulp.task('default', function() {
-    gulp.run('browserify', 'views', 'compass');
-});
+//package all files for release
+gulp.task('default', ['browserify', 'views', 'compass']);
